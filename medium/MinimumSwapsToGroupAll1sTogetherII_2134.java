831809edeaecd0e748a8d64133cec50e31cde0d7
FernandoCruzCavina <fernando.cruz.cavina@gmail.com>
2025-10-09 17:10:33 -0300

    add ex-125,2134,2537

---
+++ medium/MinimumSwapsToGroupAll1sTogetherII_2134.java
@@ -0,0 +1,20 @@
+package medium;
+
+import java.util.Arrays;
+
+public class MinimumSwapsToGroupAll1sTogetherII_2134 {
+    public int minSwaps(int[] nums) {
+        int k = Arrays.stream(nums).sum();
+        int n = nums.length;
+        int count = 0;
+        for (int i = 0; i < k; ++i) {
+            count += nums[i];
+        }
+        int mx = count;
+        for (int i = k; i < n + k; ++i) {
+            count += nums[i % n] - nums[(i - k + n) % n];
+            mx = Math.max(mx, count);
+        }
+        return k - mx;
+    }
+}
